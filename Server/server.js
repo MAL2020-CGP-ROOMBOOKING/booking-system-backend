eaf986b74d2d2a24920e5739466780c28181ed06
TanggaPendek <133072986+TanggaPendek@users.noreply.github.com>
2025-02-05 15:06:13 +0800

    Api rework
    user
    -get
    -post
    -put
    -del

--- Server/server.js
+++ Server/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
-const { connectDB } = require("./config/db");
+const { connectDB } = require("../config/db");
 
 dotenv.config();
 const app = express();
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cors());
 
 connectDB().then(() => {
-    app.use("/api/users", require("./routes/userRoutes"));
+    app.use("/api/users", require("../routes/userRoutes"));
 
     app.listen(PORT, () => console.log(`🚀 Server running at http://localhost:${PORT}`));
 });
