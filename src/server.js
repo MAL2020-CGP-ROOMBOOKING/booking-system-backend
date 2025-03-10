8fcee316692e2a090c21aed04e33c51ec30de4bc
Wong-Kai-Lun <kai.l.wong@students.plymouth.ac.uk>
2025-03-10 23:26:37 +0800

    Updated top navigation bar
    .ejs and .css

--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 const express = require("express");
 const session = require('express-session');
-// const cors = require("cors");
 const { connectDB, client } = require("./config/db");
 const { authenticateToken } = require("./middleware/authMiddleware");
 const MongoStore = require("connect-mongo")
@@ -10,12 +9,10 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 /* Middleware */
-// app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
-
 app.use(session({
     store: MongoStore.create({client, dbName: process.env.DB_NAME}),
     secret: 'discoKingCave1111',
